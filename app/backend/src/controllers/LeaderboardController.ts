8d2c15535a3639c467fba04bb87d6f7a2b34c124
Gabriela Muniz <gabrielanamu@live.com>
2023-05-12 18:28:16 -0300

    feat: add req24 and req25 - add atributes in req23 and order the return

--- app/backend/src/controllers/LeaderboardController.ts
+++ app/backend/src/controllers/LeaderboardController.ts
@@ -3,7 +3,7 @@ import LeaderboardService from '../services/LeaderboardService';
 
 export default class LeaderboardController {
   public static async getAll(req: Request, res: Response) {
-    const teams = await LeaderboardService.getAll();
+    const teams = await LeaderboardService.sort();
     return res.status(200).json(teams);
   }
 }
--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -17,20 +17,29 @@ export default class LeaderboardService {
         totalLosses: this.getTotalLosses(matches),
         goalsFavor: this.getGoalsFavor(matches),
         goalsOwn: this.getGoalsOwn(matches),
+        goalsBalance: this.getGoalsBalance(matches),
+        efficiency: this.getEfficiency(matches),
       };
     }));
   }
 
   public static getTotalPoints(matches: MatcheAtributes[]) {
-    const result = matches.reduce((acc, el) => (el.homeTeamGoals < el.awayTeamGoals
-      ? acc + 0 : acc + el.homeTeamGoals), 0);
-    // const result = (this.getTotalVictories(matches) * 3) + this.getTotalDraws(matches);
+    const result = matches.reduce((acc, el) => {
+      if (el.homeTeamGoals < el.awayTeamGoals) {
+        return acc;
+      }
+      if (el.homeTeamGoals > el.awayTeamGoals) {
+        const points = acc + 3;
+        return points;
+      }
+      return acc + 1;
+    }, 0);
     return result;
   }
 
   public static getTotalVictories(matches: MatcheAtributes[]) {
-    const result = matches.reduce((acc, el) => (el.homeTeamGoals < el.awayTeamGoals
-      ? acc + 0 : acc + 1), 0);
+    const result = matches.reduce((acc, el) => (el.homeTeamGoals > el.awayTeamGoals
+      ? acc + 1 : acc), 0);
     return result;
   }
 
@@ -54,5 +63,33 @@ export default class LeaderboardService {
   public static getGoalsOwn(matches: MatcheAtributes[]) {
     const result = matches.reduce((acc, el) => (acc + el.awayTeamGoals), 0);
     return result;
+  }
+
+  public static getGoalsBalance(matches: MatcheAtributes[]) {
+    const result = matches.reduce((acc, el) => acc + (el.homeTeamGoals - el.awayTeamGoals), 0);
+    return result;
+  }
+
+  public static getEfficiency(matches: MatcheAtributes[]) {
+    const totalPoints = this.getTotalPoints(matches);
+    const totalGames = matches.length;
+    const result = ((totalPoints / (totalGames * 3)) * 100).toFixed(2);
+    return result;
+  }
+
+  public static async sort() {
+    const m = await this.getAll();
+    m.sort((b, a) => {
+      if (a.totalPoints === b.totalPoints) {
+        if (a.totalVictories === b.totalVictories) {
+          if (a.goalsBalance === b.goalsBalance) {
+            return a.goalsFavor - b.goalsFavor;
+          }
+          return a.goalsBalance - b.goalsBalance;
+        }
+        return a.totalVictories - b.totalVictories;
+      }
+      return a.totalPoints - b.totalPoints;
+    }); return m;
   }
 }
